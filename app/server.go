9faedd5e33fb369828958581d4a48ebae14de5a3
root <root@localhost>
2024-04-22 13:26:27 +0000

    Adding initial implementation of PING command

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,9 +13,33 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	handleConnection(conn)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	readBuffer := make([]byte, 1024)
+	_, err := conn.Read(readBuffer)
+	if err != nil {
+		fmt.Println("Unable to read message from connection")
+		os.Exit(1)
+	}
+	requestContent := string(readBuffer)
+	var response []byte
+	if strings.Contains(strings.ToLower(requestContent), "ping") {
+		response = []byte("+PONG\r\n")
+	}
+
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Println("found an error trying to respond")
+		return
 	}
 }
