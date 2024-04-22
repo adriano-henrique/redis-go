9a5374a17f037182b3cd0150aefb5f35f370a4f1
root <root@localhost>
2024-04-22 13:38:31 +0000

    allowing system to read multiple ping commands

--- app/server.go
+++ app/server.go
@@ -14,32 +14,37 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	handleConnection(conn)
+		handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	readBuffer := make([]byte, 1024)
-	_, err := conn.Read(readBuffer)
-	if err != nil {
-		fmt.Println("Unable to read message from connection")
-		os.Exit(1)
-	}
-	requestContent := string(readBuffer)
-	var response []byte
-	if strings.Contains(strings.ToLower(requestContent), "ping") {
-		response = []byte("+PONG\r\n")
-	}
+	for {
+		_, err := conn.Read(readBuffer)
+		if err != nil {
+			fmt.Println("Unable to read message from connection")
+			os.Exit(1)
+		}
+		fmt.Printf("Received: %s\n", string(readBuffer))
+		requestContent := string(readBuffer)
+		var response []byte
+		if strings.Contains(strings.ToLower(requestContent), "ping") {
+			response = []byte("+PONG\r\n")
+		}
 
-	_, err = conn.Write(response)
-	if err != nil {
-		fmt.Println("found an error trying to respond")
-		return
+		_, err = conn.Write(response)
+		if err != nil {
+			fmt.Println("found an error trying to respond")
+			return
+		}
 	}
 }
